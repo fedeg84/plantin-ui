8a3c77964f09ee198010a1631cd506385ecc3db1
fedeg84 <fedeg84@gmail.com>
2025-08-06 16:42:08 -0300

    Remove attributes column from product types list table

--- src/pages/ProductTypesPage.tsx
+++ src/pages/ProductTypesPage.tsx
@@ -174,9 +174,7 @@ export default function ProductTypesPage() {
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Tipo Padre
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Atributos
-                    </th>
+                    
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Creado por
                     </th>
@@ -204,11 +202,7 @@ export default function ProductTypesPage() {
                           {productType.parent_name || '-'}
                         </div>
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {productType.attributes?.length || 0} atributos
-                        </div>
-                      </td>
+                      
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="text-sm text-gray-900">{productType.created_by_username}</div>
                       </td>
